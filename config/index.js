e83385644f6170e864c665228aa55495007765a0
Ernesto Hurtado <calerojulio92@gmail.com>
2022-06-17 17:37:55 -0600

    eliminando index.js de config/

--- config/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  PORT: process.env.PORT || 3002,
-  MONGO_URI: process.env.MONGO_URI || "mongodb://127.0.0.1:27017/cafe_db",
-  SECRET_JWT:
-    process.env.SECRET_JWT ||
-    "1d8c862c4f39cba526730e401fc33377270a2e4a2bc62fb457c20e4de4c04147",
-};
