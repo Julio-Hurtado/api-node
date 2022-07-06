55b77ba85159c99b6e933736f075e8c754540300
Ernesto Hurtado <calerojulio92@gmail.com>
2022-07-05 19:04:29 -0600

    expose route search

--- Routes/index.js
+++ Routes/index.js
@@ -2,10 +2,11 @@ const userRoutes = require("./user.route");
 const authRoutes = require("./auth.route");
 const categoryRoutes = require("./category.route");
 const productRoutes = require("./product.route");
-
+const  searchRoutes= require("./search.route")
 module.exports = {
   authRoutes,
   categoryRoutes,
   productRoutes,
+  searchRoutes,
   userRoutes,
 };
