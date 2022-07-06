d21ccac54c85eb11ea90d4e06db4ad0093d6859e
Ernesto Hurtado <calerojulio92@gmail.com>
2022-07-05 18:55:28 -0600

    refactoring method for return  errors custom

--- Middlewares/validate-data.js
+++ Middlewares/validate-data.js
@@ -3,13 +3,12 @@ const Response = require("../Network/response");
 const ValidateError = require("../Models/validateError");
 
 const validateData = (req, res, next) => {
-  let resErros = [];
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    errors.array().map((err) => {
-      resErros = [...resErros, new ValidateError(err.param, err.msg)];
-    });
-    return Response.error(req, res, {errors: resErros}, 400);
+    const valErrors = errors
+      .array()
+      .map((err) => new ValidateError(err.param, err.msg));
+    return Response.error(req, res, {errors: valErrors}, 400);
   }
   next();
 };
