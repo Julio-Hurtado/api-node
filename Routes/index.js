8e83b248e7e5d8659e187fc0a21bf98d753b18c5
Ernesto Hurtado <calerojulio92@gmail.com>
2022-07-11 19:42:31 -0600

    add and export  route upload

--- Routes/index.js
+++ Routes/index.js
@@ -3,10 +3,12 @@ const authRoutes = require("./auth.route");
 const categoryRoutes = require("./category.route");
 const productRoutes = require("./product.route");
 const  searchRoutes= require("./search.route")
+const uploadRoutes=require("./uploads.route")
 module.exports = {
   authRoutes,
   categoryRoutes,
   productRoutes,
   searchRoutes,
   userRoutes,
+  uploadRoutes
 };
