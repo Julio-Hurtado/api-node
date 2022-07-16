582d912ae89be7275ebfd95aaf519db3add9354c
Ernesto Hurtado <calerojulio92@gmail.com>
2022-07-16 13:15:42 -0600

    modify location  files to export

--- Middlewares/index.js
+++ Middlewares/index.js
@@ -1,11 +1,12 @@
-const validateData = require("../Middlewares/validate-data");
-const validateJwt = require("../Middlewares/validate_jwt");
-const validateRole = require("../Middlewares/validate_role");
-const globalErrors = require("../Middlewares/errors");
-
+const validateData = require("./validate-data");
+const validateJwt = require("./validate_jwt");
+const validateRole = require("./validate_role");
+const globalErrors = require("./errors");
+const validatedFile = require("./validate_fileUp");
 module.exports = {
   ...validateData,
   ...validateJwt,
   ...validateRole,
+  ...validatedFile,
   globalErrors,
 };
